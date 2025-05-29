b4f7c9470db12fa8fdac7e61218d173c9ea9b160
Minsung Kim <tkfpsk@gmail.com>
2025-05-29 15:10:35 +0100

    Stage 5: Contact and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import NavBar from './components/NavBar'
 import Hero from './sections/Hero'
 import Showcase from './sections/ShowcaseSection'
 import Experience from './sections/Experience'
+import TechStack from './sections/TechStack'
+import Testimonials from './sections/Testimonials'
+import Contact from './sections/Contact'
+import Footer from './sections/Foorter'
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <Hero />
       <Showcase />
       <Experience />
+      <TechStack />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   )
 }
